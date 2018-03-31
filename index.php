d37c072add2574d3e4ef4919a0e084c042357aa0
authuser0603 <37950679+authuser0603@users.noreply.github.com>
2018-03-31 01:53:38 -0700

    Update index.php

--- index.php
+++ index.php
@@ -1,14 +1,16 @@
 <?php
 $validate_key = '123456789';
-if(isset($_REQUEST['key']) && $_REQUEST['key'] != ''{
-	if($validate_key == $_REQUEST['key'])
+if(isset($_REQUEST['key']) && $_REQUEST['key'] != ''){
+	if($validate_key == $_REQUEST['key']){
 		$RrtObj->validation = 'true';
+	}
+	else{
+		$RrtObj->validation = 'false';
+	}
 }
 else{
 	$RrtObj->validation = 'false';
 }
 
 $reJSON = json_encode($RrtObj);
-
-echo $reJSON;
 ?>
