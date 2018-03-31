33486e7da6f69c49bda43833154142a1a23175c8
authuser0603 <37950679+authuser0603@users.noreply.github.com>
2018-03-31 02:32:08 -0700

    Update index.php

--- index.php
+++ index.php
@@ -1,19 +1,1 @@
-<?php
-$validate_key = '123456789';
-$RrtObj = new stdClass();
-if(isset($_REQUEST['key']) && $_REQUEST['key'] != ''){
-	if($validate_key == $_REQUEST['key']){
-		$RrtObj->validate = 'Allow';
-	}
-	else{
-		$RrtObj->validate = 'Disallow';
-	}
-}
-else{
-	$RrtObj->validate = 'Disallow';
-}
 
-$reJSON = json_encode($RrtObj);
-
-echo $reJSON;
-?>
